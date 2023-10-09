8acd1cec9d947f63429507fed7cf2b82483a9b12
shun-09 <mcilal000@gmail.com>
2023-10-09 17:13:44 +0900

    userモデルバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  with_options presence: true do
+    validates :nickname
+    validates :birth_date
+  end
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶー一-龠々]+\z/} do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/} do
+    validates :kana_last_name
+    validates :kana_first_name
+  end
+
+
 end
