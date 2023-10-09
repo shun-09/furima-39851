9130174c2cf782d09a1845ddbcd795c5a33ec68f
shun-09 <mcilal000@gmail.com>
2023-10-09 18:24:41 +0900

    passwordにバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,20 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
+  VALID_FORMAT_JAPANESE = /\A[ぁ-んァ-ヶー一-龠々]+\z/
+  VALID_FORMAT_KANA = /\A[ァ-ヶー]+\z/
+
   with_options presence: true do
     validates :nickname
+    validates :last_name, format: { with: VALID_FORMAT_JAPANESE }
+    validates :first_name, format: { with: VALID_FORMAT_JAPANESE }
+    validates :kana_last_name, format: { with: VALID_FORMAT_KANA }
+    validates :kana_first_name, format: { with: VALID_FORMAT_KANA }
     validates :birth_date
   end
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶー一-龠々]+\z/} do
-    validates :last_name
-    validates :first_name
-  end
-
-  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/} do
-    validates :kana_last_name
-    validates :kana_first_name
-  end
-
-
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  
 end
