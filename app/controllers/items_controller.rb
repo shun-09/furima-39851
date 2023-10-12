705f8dc665b593a6270a8b0236dbb22638980fbf
shun-09 <mcilal000@gmail.com>
2023-10-12 21:17:03 +0900

    保存設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,21 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :name, :description, :genre_id, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :period_id, :price, :image
+    ).merge(user_id: current_user.id)
+  end
+
 end
