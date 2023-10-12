025aad032bcf39635d1b8594cb8cf4e51ebdd1d1
shun-09 <mcilal000@gmail.com>
2023-10-12 21:28:37 +0900

    エラーメッセージの表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,8 +9,10 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :period
 
-  validates :name, :description, presence: true
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range" }
-  validates :price, presence: true, numericality: { only_integer: true, message: "is invalid. Input half-width characters" }
-  validates :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :period_id, numericality: { other_than: 1, message: "can't be blank" } 
+  with_options presence: true do
+    validates :name, :description, :image
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range" }
+    validates :price, numericality: { only_integer: true, message: "is invalid. Input half-width characters" }
+    validates :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :period_id, numericality: { other_than: 1, message: "can't be blank" } 
+  end
 end
