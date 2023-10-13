09f1bd2964f42f1a2cc3502d3a725deb027e5365
shun-09 <mcilal000@gmail.com>
2023-10-13 11:18:57 +0900

    販売手数料と販売利益の表示

---
+++ app/javascript/calculator.js
@@ -0,0 +1,13 @@
+function count (){
+  const itemPrice = document.getElementById("item-price");
+  itemPrice.addEventListener("keyup", () => {
+    const taxPrice = Math.floor( itemPrice.value * 0.1 );
+    const addTax = document.getElementById("add-tax-price");
+    addTax.innerHTML = taxPrice;
+    const profit = Math.floor( itemPrice.value - taxPrice );
+    const addProfit = document.getElementById("profit");
+    addProfit.innerHTML = profit;    
+  });
+};
+
+window.addEventListener('turbo:load', count);
