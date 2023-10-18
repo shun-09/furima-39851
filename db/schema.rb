9b675f46cc1292fbb7c78e1a2ea066000e30a421
shun-09 <mcilal000@gmail.com>
2023-10-18 22:17:32 +0900

    addressテーブルダウン

--- db/schema.rb
+++ db/schema.rb
@@ -39,19 +39,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_16_095319) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "addresses", charset: "utf8", force: :cascade do |t|
-    t.string "postal_code", null: false
-    t.integer "prefecture_id", null: false
-    t.string "municipality", null: false
-    t.string "block", null: false
-    t.string "building_name"
-    t.string "phone_number", null: false
-    t.bigint "order_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["order_id"], name: "index_addresses_on_order_id"
-  end
-
   create_table "items", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.text "description", null: false
@@ -96,7 +83,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_16_095319) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "addresses", "orders"
   add_foreign_key "items", "users"
   add_foreign_key "orders", "items"
   add_foreign_key "orders", "users"
