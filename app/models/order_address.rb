16fc97061597d738b2457d220d5162995bd60112
shun-09 <mcilal000@gmail.com>
2023-10-26 01:51:38 +0900

    マニュアルのエラー文の変更

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -5,13 +5,13 @@ class OrderAddress
 
   with_options presence: true do
     validates :user_id, :item_id, :municipality, :block, :phone_number, :token
-    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "を入力してください" }
     validates :postal_code, format: {
       with: /\A\d{3}-\d{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"
     }
   end
-  validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is too short' }
-  validates :phone_number, format: { with: /\A\d+\z/, message: ' is invalid. Input only number' }
+  validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'が短いです' }
+  validates :phone_number, format: { with: /\A\d+\z/, message: 'は半角数字で入力してください' }
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
